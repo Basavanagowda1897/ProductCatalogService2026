f72308052d8e7f635bf0e9503da6cba0f47bceac
punith <punithg202@gmail.com>
2026-07-12 17:52:47 +0530

    #8225427-Intial Commit

--- src/main/java/com/gowda/productcatalogservice2026/configurations/RestTemplateConfig.java
+++ src/main/java/com/gowda/productcatalogservice2026/configurations/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package com.gowda.productcatalogservice2026.configurations;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
